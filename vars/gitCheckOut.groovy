33c26af7184b70619a506589ac6c9546ec4ed23c
kakarot <kakarotzcloud@gmail.com>
2026-02-23 17:17:43 +0530

    Fix parameter usage in git function

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -1,3 +1,3 @@
-def git(string branch, string url, string credentialsId){
-  git branch: 'branch' credentialsId: 'credentialsId', url: 'url'
+def git(String branch, String url, String credentialsId){
+  git branch: branch credentialsId: credentialsId, url: url
 }
