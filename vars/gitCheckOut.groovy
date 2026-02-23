3dbc28915624e5105f20cc992f1265f975de31d0
kakarot <kakarotzcloud@gmail.com>
2026-02-23 17:34:07 +0530

    Reorder parameters in gitCheckOut function

--- vars/gitCheckOut.groovy
+++ vars/gitCheckOut.groovy
@@ -1,3 +1,3 @@
-def call(String branch, String url, String credentialsId){
+def call(String branch, String credentialsId, String url){
   git branch: branch, credentialsId: credentialsId, url: url
 }
